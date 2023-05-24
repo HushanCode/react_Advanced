6376eb142e7617ca99c855498366951b7bb686fd
zhuling822 <ling.zhu@js.design>
2023-05-24 20:00:31 +0800

    feat: react进阶实战组件篇

---
+++ 02components/demo/ComCommunicate/Propsdemo/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Father from './Father';
+
+function App() {
+  return (
+    <Father></Father>
+  );
+}
+
+export default App;
---
+++ 02components/demo/CompExpansion/extendComp/Propsdemo/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Programmer from './Programmer';
+
+function App() {
+  return (
+    <Programmer></Programmer>
+  );
+}
+
+export default App;
